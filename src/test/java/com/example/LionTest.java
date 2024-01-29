bc977e9a3e75a66eb0504e739c44f5788b291903
Regina Zinnatullina <reginaezinnatullina@yandex.ru.com>
2024-01-29 21:45:08 +0300

    Задание по 6 спринту - после 2-х правок

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -5,8 +5,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.assertEquals;
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
     @Mock
@@ -14,21 +12,18 @@ public class LionTest {
     private final int numberOfKittens = 1;
     @Test
     public void getFoodTest() throws Exception {
-        Lion lion = new Lion("Самка",feline);
+        Lion lion = new Lion("Самка", feline);
         lion.getFood();
         Mockito.verify(feline).getFood(Mockito.anyString());
-      //  assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", lion.getFood(), ("Животные, Птицы, Рыба"));
     }
     @Test
     public void getKittensTest() throws Exception {
         Lion lion = new Lion("Самец", feline);
-        int actual = lion.getKittens();
-        assertEquals(numberOfKittens, 1);
+        lion.getKittens();
+        Mockito.verify(feline).getKittens();
     }
-
     @Test(expected = Exception.class)
     public void doesHaveManeTestThrowException() throws Exception{
         Lion lion = new Lion("Дитя", feline);
-
     }
 }
